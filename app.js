4ae3ad7746196b07f93fbb928c42d3538724f3e5
Kevin Zamzami <kevin.zamzami@hashmicro.id>
2023-04-04 00:19:58 +0700

    fix date format

--- app.js
+++ app.js
@@ -90,9 +90,15 @@ app.post("/", async function (request, response) {
                                         });
                                         const article = new Readability(dom.window.document).parse();
                                         const content = article ? article.textContent : newsData.articles[0].content;
+                                        const publishedAt = newsData.articles[0].publishedAt;
+                                        const date = new Date(publishedAt);
+
+                                        const year = date.getFullYear();
+                                        const month = String(date.getMonth() + 1).padStart(2, "0"); 
+                                        const day = String(date.getDate()).padStart(2, "0");
                                         resolve({
                                             news_title: newsData.articles[0].title,
-                                            news_publish_date: newsData.articles[0].publishedAt,
+                                            news_publish_date: `${day}-${month}-${year}`,
                                             news_link: newsData.articles[0].url,
                                             news_content: content
                                         });
